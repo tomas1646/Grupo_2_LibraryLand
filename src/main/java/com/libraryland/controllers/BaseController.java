ebffe4b96be4dda9b8238770dd21083cbd1fa28f
JokinAbarzua <jokinabarzua22@hotmail.com>
2022-10-21 17:13:18 -0300

    Seguridad Basica

--- src/main/java/com/libraryland/controllers/BaseController.java
+++ src/main/java/com/libraryland/controllers/BaseController.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.Serializable;
 
@@ -15,4 +16,7 @@ public interface BaseController <E extends Base, ID extends Serializable>{
     public ResponseEntity<?> save(@RequestBody E entity);
     public ResponseEntity<?> update(@PathVariable ID id, @RequestBody E entity);
     public ResponseEntity<?> delete(@PathVariable ID id);
+    public ResponseEntity<?> search(@RequestParam String filtro);
+    public ResponseEntity<?> search(@RequestParam String filtro, Pageable pageable);
+
 }
