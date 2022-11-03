b84ecfcc0b0021478187fff7ddbeeb180e0851fa
Tomas Espinosa <tomasespin12@gmail.com>
2022-11-03 12:18:59 -0300

    Busqueda y Detalle simple

--- src/main/java/com/libraryland/controllers/Book_Controller.java
+++ src/main/java/com/libraryland/controllers/Book_Controller.java
@@ -5,39 +5,50 @@ import com.libraryland.services.BookServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Map;
 
 @Controller
 @CrossOrigin(origins = "*")
-@RequestMapping(value="books")
+@RequestMapping(value = "")
 public class Book_Controller {
 
     @Autowired
     protected BookServiceImpl bookService;
 
     @GetMapping("")
-    public String index(@RequestParam Map<String,Object> params, Model model){
-        int page = params.get("page") != null ? (Integer.valueOf(params.get("page").toString())-1):0;
-        PageRequest pageRequest = PageRequest.of(page,20);
-        Page<Book> books = null;
-        try {
-            books = bookService.findAll(pageRequest);
-            model.addAttribute("books", books);
-            model.addAttribute("totalPages", books.getTotalPages());
+    public String index(@RequestParam Map<String, Object> params, Model model) {
+        return "index";
+    }
 
-            return "index";
+    @GetMapping("/search")
+    public String search(@RequestParam Map<String, Object> params, Model model) {
+        int page = params.get("page") != null ? (Integer.valueOf(params.get("page").toString()) - 1) : 0;
+        PageRequest pageRequest = PageRequest.of(page, 20);
+        Page<Book> books = null;
+
+        try {
+            books = bookService.search(params.get("filtro").toString(), pageRequest);
+            model.addAttribute("books", books);
+
+            return "views/search";
         } catch (Exception e) {
             return "error";
         }
+    }
 
+    @GetMapping("/detail/{id}")
+    public String detail(@PathVariable("id") Long id, Model model) {
+        try {
+            Book book = bookService.findById(id);
+            model.addAttribute("book", book);
+
+            return "views/detail";
+        } catch (Exception e) {
+            return "error";
+        }
     }
 }
