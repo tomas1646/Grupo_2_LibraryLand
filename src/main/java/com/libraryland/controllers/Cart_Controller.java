61bfa3c60a2f8babc21505e822125c9bfa24d5a2
JokinAbarzua <jokinabarzua22@hotmail.com>
2022-11-10 01:18:27 -0300

    Ver Carrito (falta logica de cambiar cantidades y eliminar detalles)

--- src/main/java/com/libraryland/controllers/Cart_Controller.java
+++ src/main/java/com/libraryland/controllers/Cart_Controller.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +36,37 @@ public class Cart_Controller {
     @Autowired
     protected UserServiceImpl userService;
 
+    @GetMapping("/findCart")
+    public String findCart(Model model, Authentication authentication) {
+        if (authentication != null){            
+            try {
+                
+                String username =  authentication.getName();
+                Optional<User> currentUser = userService.findByName(username);
+                if(currentUser.isPresent()){
+                    User user = currentUser.get();
+                    Cart cart = user.getCart();                                        
+                    List<CartDetail> allCartDetails= cart.getDetails();                    
+
+                    model.addAttribute("cart", cart);
+                    double total = 550;
+                    for(int i = 0;i < allCartDetails.size();i++){
+                        total = total + allCartDetails.get(i).getPrice();
+                    }                                               
+                    model.addAttribute("total", total);            
+                }else{
+                    throw new Exception("El usuario no se encuentra en la base de datos");
+                }
+                return "views/cart";
+                
+            } catch (Exception e) {
+                return "error";
+            }
+        }else{
+            return "redirect:/login";
+        }
+    }
+
     @PostMapping("/addToCart/{id}")
     public String addToCart(@PathVariable("id") Long id, Model model,Authentication authentication){        
         if (authentication != null){            
@@ -46,16 +78,20 @@ public class Cart_Controller {
                     User user = currentUser.get();
                     Cart cart = user.getCart();
                     Book book = bookService.findById(id);
-                    CartDetail cartDetail = CartDetail.builder().cart(cart).book(book).price(book.getPrice()).quantity(1).build();
-                    List<CartDetail> allCartDetails= cart.getDetails();
-                    allCartDetails.add(cartDetail);
-                    cart.setDetails(allCartDetails);
-                    cart.setQuantity(cart.getQuantity() + 1);
-                    cartService.update(cart.getId(), cart);
+                    if(book.getStock() < 1){
+                        throw new Exception("No hay stock");    
+                    }else{
+                        CartDetail cartDetail = CartDetail.builder().cart(cart).book(book).price(book.getPrice()).quantity(1).build();
+                        List<CartDetail> allCartDetails= cart.getDetails();
+                        allCartDetails.add(cartDetail);
+                        cart.setDetails(allCartDetails);
+                        cart.setQuantity(cart.getQuantity() + 1);
+                        cartService.update(cart.getId(), cart);
+                    }                                        
                 }else{
                     throw new Exception("El usuario no se encuentra en la base de datos");
                 }
-                return "redirect:/index";
+                return "redirect:/";
                 
             } catch (Exception e) {
                 return "error";
